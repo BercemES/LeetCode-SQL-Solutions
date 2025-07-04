a0473ebab6f97cb9bd3ab6b29388289a84228132
BercemES <159828843+BercemES@users.noreply.github.com>
2025-07-04 09:39:59 +0300

    Update combine_two_tables.sql

--- combine_two_tables.sql
+++ combine_two_tables.sql
@@ -27,6 +27,9 @@ values
 select * from person;
 select * from address;
 
+/* Write a solution to report the first name, last name, city, and state of each person in the Person table. 
+If the address of a personId is not present in the Address table, report null instead.
+Return the result table in any order. */
 #Cozum
 select A.firstName, A.lastName, B.city, B.state from person A
 LEFT JOIN address B
