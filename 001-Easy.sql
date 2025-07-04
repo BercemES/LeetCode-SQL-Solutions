4a2570f63e5d53a6741c1e964fe5b7f5626deccd
BercemES <159828843+BercemES@users.noreply.github.com>
2025-07-04 19:44:44 +0300

    Update and rename 001.sql to 001-Easy.sql

--- 001-Easy.sql
+++ 001-Easy.sql
@@ -45,4 +45,33 @@ Return the result table in any order.*/
 #Cozum
 SELECT email from Person
 GROUP BY email
-HAVING COUNT(email) > 1; 
+HAVING COUNT(email) > 1;
+
+/*Write a solution to find the employees who earn more than their managers.
+Return the result table in any order.*/
+
+#Cozum
+SELECT E.name AS Employee FROM Employee E
+JOIN Employee M 
+ON E.managerId = M.id
+WHERE E.salary > M.salary;
+
+/*Write a solution to find all customers who never order anything.
+Return the result table in any order.*/
+
+#Cozum
+SELECT name AS Customers FROM Customers A
+LEFT JOIN Orders B ON A.id = B.CustomerId
+WHERE B.customerId IS NULL;
+
+
+/*Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
+After running your script, the answer shown is the Person table.
+The driver will first compile and run your piece of code and then show the Person table. The final order of the Person table does not matter.*/
+
+#Cozum
+DELETE p1 FROM Person p1
+LEFT JOIN Person p2
+ON p1.email = p2.email AND p1.id > p2.id
+WHERE p2.id IS NOT NULL;
